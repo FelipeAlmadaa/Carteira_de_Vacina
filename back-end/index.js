480a67058508372d94ea69d1b0e77da7e84d54e9
FelipeAlmadaa <felipealmada1403@yahoo.com>
2025-06-30 23:46:22 -0300

    fix do fix

--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,19 @@
-import app from './app.js';
-import { sequelize } from './config/db.js';
+// import app from './app.js';
+// import { sequelize } from './config/db.js';
 
-async function start() {
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    console.log('🎉 Banco conectado');
+// async function start() {
+//   try {
+//     await sequelize.authenticate();
+//     await sequelize.sync();
+//     console.log('🎉 Banco conectado');
 
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Erro ao conectar com banco:', error);
-  }
-}
+//     const PORT = process.env.PORT || 3000;
+//     app.listen(PORT, () => {
+//       console.log(`Servidor rodando na porta ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error('Erro ao conectar com banco:', error);
+//   }
+// }
 
-start();
+// start();
