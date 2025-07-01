96b152cec7bc83819d6d3a1803c9330f902aef84
FelipeAlmadaa <felipealmada1403@yahoo.com>
2025-07-01 00:03:37 -0300

    tentando vercel novamente

--- back-end/index.js
+++ back-end/index.js
@@ -1,20 +1,21 @@
 import app from './app.js';
-
 import { sequelize } from './config/db.js';
+import './models/User.js';
+import './models/Vaccine.js';
+import './models/UserVaccine.js';
+import './models/associations.js';
 
-
+const PORT = process.env.PORT || 3000;
 
 try {
-await sequelize.authenticate();
-await sequelize.sync();
-console.log('🎉 Banco conectado');
+  await sequelize.authenticate();
+  await sequelize.sync();
+  console.log('🎉 Banco conectado');
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  });
 
 } catch (error) {
-
-console.error('Erro ao conectar com banco:', error);
-
+  console.error('❌ Erro ao iniciar servidor:', error);
 }
-
-
-
-export default app;
