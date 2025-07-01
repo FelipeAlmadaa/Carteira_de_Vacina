1cef14fddeecac1d7b46c50c06c9d554b4b66dc2
FelipeAlmadaa <felipealmada1403@yahoo.com>
2025-06-30 22:59:31 -0300

    tentativa 6

---
+++ index.js
@@ -0,0 +1,19 @@
+import app from './app.js';
+import { sequelize } from './config/db.js';
+
+async function start() {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log('🎉 Banco conectado');
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Erro ao conectar com banco:', error);
+  }
+}
+
+start();
